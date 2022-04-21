ef4d56004159f3c73500242d894fe074f4c95971
Ann <49995044+AnaSudame@users.noreply.github.com>
2022-04-21 14:52:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+var slider = document.getElementById("myRange");
+ var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+// Code for input validation: email
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+
+
+
+
+	// if valid, submit the form.
+	if (nameValid && emailValid) {
+    // alert("Demo only. No form was posted.");
+		 // alert(  "value: " + document.querySelector('input[type=range][id=myRange]').value) ;
+      if ((document.querySelector('input[type=range][id=myRange]').value) < 7){
+    alert("You need a vac to your fav getaway : " + document.querySelector('input[type=radio][name=fav_vac]:checked').value);
+  }   else {alert("Keep going! You got this!!")}
+
+	}
+});
+
+//test this function
+function ValidateGoalSelection()
+{
+    var checkboxes = document.getElementsByName("goal");
+    var numberOfCheckedItems = 0;
+    for(var i = 0; i < checkboxes.length; i++)
+    {
+        if(checkboxes[i].checked)
+            numberOfCheckedItems++;
+    }
+    if(numberOfCheckedItems > 2)
+    {
+        alert("You can only select two goals!");
+        return false;
+    }
+}
